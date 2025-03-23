2d85825bfe7a006a0a8d7c09f55deb9af13fc292
ShaikhMasud <shaikhmasud507@gmail.com>
2025-03-23 23:06:56 +0530

    first commit

---
+++ Social_Media_Addiction_Predictor_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SocialMediaAddictionPredictorAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Social_Media_Addiction_Predictor_app'
